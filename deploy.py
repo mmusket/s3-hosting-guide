fa2a9b2988e0b47e3e23e4e8282b76a7a9887f37
Marco Muscat <marco.muscat@gmail.com>
2016-08-03 23:06:51 +0100

    Cleaned up script

--- deploy.py
+++ deploy.py
@@ -1,21 +1,21 @@
 from __future__ import print_function
-import gzip
 import os
 import boto3
-import argparse
-import sys
-import pathlib
 from botocore.exceptions import ClientError
+from mimetypes import MimeTypes
+import gzip
+from shutil import copyfile
 import threading
 from multiprocessing.pool import ThreadPool
-from mimetypes import MimeTypes
-from shutil import copyfile
 
 dontZip = ['.jpg','.png','.ttf','.woff','.woff2','.gif']
+bucketName = 'blank-website'
+sourceDir = 'dist'
+stagingDir = 'out\\'
 
-def getFiles(start):
+def getFiles(baseFolder):
     file_paths = []   
-    for root, directories, files in os.walk(start):
+    for root, directories, files in os.walk(baseFolder):
         for filename in files:         
             filepath = os.path.join(root, filename)
             file_paths.append(filepath)  
@@ -48,13 +48,12 @@ def upload_file(bucket_name,filePath):
     s3 = session.resource('s3')
     destname = os.path.join(*(filePath.split('\\')[2:])).replace('\\','/')          
     print ("Uploading file " + filePath + ' to ' + destname)
-    try:
-        #output = client.upload_file(filePath, bucket_name, destname)           
+    try:      
         data = open(filePath, 'rb')
         ftype, encoding = MimeTypes().guess_type(filePath)
         conType = ftype if ftype is not None else encoding if encoding is not None else 'text/plain'    
-        encType =  'gzip' if isZipFile(filePath) else None  
-        s3.Object(bucket_name, destname).put(Body=data,ContentEncoding='gzip',ContentType=conType,ACL='public-read')
+        encType =  'gzip' if isZipFile(filePath) else ''  
+        s3.Object(bucket_name, destname).put(Body=data,ContentEncoding=encType,ContentType=conType,ACL='public-read')
     except ClientError as err:
         print("Failed to upload artefact to S3.\n" + str(err))
         return False
@@ -63,51 +62,6 @@ def upload_file(bucket_name,filePath):
         return False   
     return True
 
-
-# def upload_to_s3(bucket_name, sourceDir, bucket_key):
-#     print("Starting script")  
-#     session = boto3.Session(profile_name='deploy')
-#     client = session.client('s3')
-#     uploadFileNames = getFiles(sourceDir)
-#     print("Found " + len(uploadFileNames).__str__() + ' files')
-#     for filename in uploadFileNames:      
-#         try:
-#             #filename = filename.replace('\\','/') #if on windows s3 wont seperate files in folders        
-#             destname = os.path.join(*(filename.split('\\')[2:])).replace('\\','/')          
-#             print ("Uploading file " + filename + ' to ' + destname)
-#             client.upload_file(filename, bucket_name, destname)
-#         except ClientError as err:
-#             print("Failed to upload artefact to S3.\n" + str(err))
-#             return False
-#         except IOError as err:
-#             print("Failed to access artefact in this directory.\n" + str(err))
-#             return False
-#     return True
-
-# def main():
-
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("bucket", help="Name of the existing S3 bucket")
-#     parser.add_argument("artefact", help="Name of the artefact to be uploaded to S3")
-#     parser.add_argument("bucket_key", help="Name of the S3 Bucket key")
-#     args = parser.parse_args()
-
-#     if not upload_to_s3(args.bucket, args.artefact, args.bucket_key):
-#         sys.exit(1)
-
-# if __name__ == "__main__":
-#     main()
-
-
-
-
-filesToProcess = getFiles('dist')
-print ('Found ' + len(filesToProcess).__str__() + ' files to process')
-[zipFile(x,'out\\' + x) if isZipFile(x) else copyFile(x,'out\\' + x) for x in filesToProcess]
-#upload_to_s3('upload-gzip-test-2','out\\dist\\','')
-#[upload_file('upload-gzip-test-2',x) for x in getFiles('out\\dist\\')]
-
-
+[zipFile(x,stagingDir + x) if isZipFile(x) else copyFile(x,stagingDir + x) for x in getFiles(sourceDir)]
 pool = ThreadPool(processes=40)
-pool.map(lambda x : upload_file('upload-gzip-test-2',x), getFiles('out\\dist\\'))
-#upload_file('upload-gzip-test-2','out\\dist\\index.html')
+pool.map(lambda x : upload_file(bucketName,x), getFiles(stagingDir))
